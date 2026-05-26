9a0d1a14fcc260cd8b0baf74ed822e37ad281a01
Julian Ordaz <1julianordaz@gmail.com>
2026-05-25 22:58:29 -0400

    Add: time-based-key-value-store - submission-5

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-5.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+
+class TimeMap:
+    def __init__(self):
+        self.dct = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.dct[key].append((timestamp, value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        sublist = self.dct[key]
+        l, r, val = 0, len(sublist)-1, ""
+
+        while l <= r:
+            m = (l + r) // 2
+            if timestamp >= sublist[m][0]:
+                val = sublist[m][1]
+                l = m + 1
+            else:
+                r = m - 1
+
+                
+        return val
+
+        
+
+                
