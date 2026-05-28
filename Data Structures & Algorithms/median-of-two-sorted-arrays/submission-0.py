347f0fd52c861d2e7cb55ad0afcbd5b4077d9b48
Julian Ordaz <1julianordaz@gmail.com>
2026-05-28 19:56:06 -0400

    Add: median-of-two-sorted-arrays - submission-0

---
+++ Data Structures & Algorithms/median-of-two-sorted-arrays/submission-0.py
@@ -0,0 +1,8 @@
+import statistics
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        nums3 = nums1 + nums2
+        nums3.sort()
+
+        return statistics.median(nums3)
