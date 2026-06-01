e1cc52509d6378447964169b3e777778e67a6c09
Julian Ordaz <1julianordaz@gmail.com>
2026-05-31 21:01:11 -0400

    Add: longest-repeating-substring-with-replacement - submission-13

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-13.py
@@ -0,0 +1,18 @@
+from collections import Counter
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        ans = 0
+        sub = []
+
+        for r in range(len(s)):
+            sub.append(s[r])
+            freqMap = Counter(sub)
+            if len(sub) - freqMap.most_common(1)[0][1] <= k:
+                ans = max(ans, len(sub))
+            else:
+                sub.pop(0)
+                freqMap = Counter(sub)
+
+            
+        return ans
