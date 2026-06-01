a4fd256d93fa8d46278335c2678fb6f5c71bfe15
Julian Ordaz <1julianordaz@gmail.com>
2026-05-31 21:03:48 -0400

    Add: longest-repeating-substring-with-replacement - submission-15

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-15.py
@@ -0,0 +1,15 @@
+from collections import Counter
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        ans, sub = 0, []
+
+        for r in range(len(s)):
+            sub.append(s[r])
+            freqMap = Counter(sub)
+            if len(sub) - freqMap.most_common(1)[0][1] <= k:
+                ans = max(ans, len(sub))
+            else:
+                sub.pop(0)
+            
+        return ans
