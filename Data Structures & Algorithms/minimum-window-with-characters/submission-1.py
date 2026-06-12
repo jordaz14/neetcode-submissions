55b422aac3ecbe9167e8c703cd7b6e8ba6d93057
Julian Ordaz <1julianordaz@gmail.com>
2026-06-11 20:11:59 -0400

    Add: minimum-window-with-characters - submission-1

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        ans = ""
+        for i in range(len(s)):
+            sub = [s[i]]
+            for j in range(i+1, len(s)):
+                sub.append(s[j])
+
+                if "".join(sorted(t)) in "".join(sorted(sub)):
+                    if ans == "" or len(sub) < len(ans):
+                        ans = "".join(sub)
+
+        return ans
