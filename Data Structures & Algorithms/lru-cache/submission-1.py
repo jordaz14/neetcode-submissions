235b3317a2d98d1cb2de41c86c338decfb3e4028
Julian Ordaz <1julianordaz@gmail.com>
2026-07-09 18:27:36 -0400

    Add: lru-cache - submission-1

---
+++ Data Structures & Algorithms/lru-cache/submission-1.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = {} # key: (val, timestamp)
+        self.capacity = capacity
+
+    def get(self, key: int) -> int:
+        if key in self.cache:    
+            self.cache[key] = (self.cache[key][0], datetime.now())
+            return self.cache[key][0]
+
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if len(self.cache) == self.capacity:
+            least_recent_key = min(self.cache, key=lambda k: self.cache[k][1]) 
+            del self.cache[least_recent_key]
+
+        self.cache[key] = (value, datetime.now())
+                
+    
